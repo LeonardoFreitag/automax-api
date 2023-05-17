3e8aca1b98ad44d0ddd032b3c395bfa83220a475
LeonardoFreitag <freitagnx@gmail.com>
2023-05-17 10:19:54 -0400

    update redis 2

--- src/config/cache.ts
+++ src/config/cache.ts
@@ -13,6 +13,8 @@ export default {
     redis: {
       host: process.env.REDIS_HOST,
       port: Number(process.env.REDIS_PORT),
+      no_ready_check: true,
+      auth_pass: process.env.REDIS_PASS,
       // password: process.env.REDIS_PASS || undefined,
     },
   },
--- src/shared/infra/http/middlewares/rateLimiter.ts
+++ src/shared/infra/http/middlewares/rateLimiter.ts
@@ -6,6 +6,8 @@ import { RateLimiterRedis } from 'rate-limiter-flexible';
 const redisClient = redis.createClient({
   host: process.env.REDIS_HOST,
   port: 56379, // Number(process.env.REDIS_PORT),
+  no_ready_check: true,
+  auth_pass: process.env.REDIS_PASS,
   // password: process.env.REDIS_PASS || undefined,
 });
 
